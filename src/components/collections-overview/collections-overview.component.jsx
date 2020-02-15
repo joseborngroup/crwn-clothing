664172a9a6f41bfd40af4f031ea4ca16f3b74683
Jose Paitamala <jose.paitamala@borngroup.com>
2020-02-15 10:13:52 +0000

    data normalization and Shop page - Collection overview

--- src/components/collections-overview/collections-overview.component.jsx
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -3,9 +3,10 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
-import { selectCollections } from '../../redux/shop/shop.selectors';
+import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
 
 import './collections-overview.styles.scss';
+
 
 const CollectionsOverview = ({ collections }) => (
   <div className='collections-overview'>
@@ -18,7 +19,7 @@ const CollectionsOverview = ({ collections }) => (
 )
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections
+  collections: selectCollectionsForPreview
 });
 
 export default connect(mapStateToProps)(CollectionsOverview);
--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -5,35 +5,35 @@ const INITIAL_STATE = {
       imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
       size: 'medium',
       id: 1,
-      linkUrl: 'hats'
+      linkUrl: 'shop/hats'
     },
     {
       title: 'jackets',
       imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
       size: 'medium',
       id: 2,
-      linkUrl: 'jackets'
+      linkUrl: 'shop/jackets'
     },
     {
       title: 'sneakers',
       imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
       size: 'medium',
       id: 3,
-      linkUrl: 'sneakers'
+      linkUrl: 'shop/sneakers'
     },
     {
       title: 'womens',
       imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
       size: 'large',
       id: 4,
-      linkUrl: 'womens'
+      linkUrl: 'shop/womens'
     },
     {
       title: 'mens',
       imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
       size: 'large',
       id: 5,
-      linkUrl: 'mens'
+      linkUrl: 'shop/mens'
     }
   ]
 }
