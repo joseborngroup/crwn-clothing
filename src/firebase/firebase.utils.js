7a3f794e5508e2e8935bf346231ebcec9bf1d7d5
Jose Paitamala <jose.paitamala@borngroup.com>
2020-02-24 22:07:48 +0000

    bringing shop data to our app

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,8 @@ const config = {
   measurementId: "G-J2ZJJYBGQQ"
 }
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
@@ -61,7 +63,20 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
   return await batch.commit()
 };
 
-firebase.initializeApp(config);
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map( doc => {
+    const { title, items  } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    }
+  });
+
+  console.log(transformedCollection);
+}
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
