93e98c4555042c378e42474226c8202b166ca6a4
Jose Paitamala <jose.paitamala@borngroup.com>
2020-03-21 21:49:49 +0000

    fix bug on category page not loading on refresh browser

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,7 +4,7 @@ import { createStructuredSelector } from 'reselect';
 import { connect } from 'react-redux';
 
 import { fetchCollectionsStartAsync } from '../../redux/shop/shop.actions';
-import { selectIsCollectionFetching } from '../../redux/shop/shop.selectors';
+import { selectIsCollectionFetching, selectIsCollectionsLoaded } from '../../redux/shop/shop.selectors';
 
 import WithSpinner from '../../components/with-spinner/with-spinner.component';
 
@@ -23,21 +23,22 @@ class ShopPage extends React.Component {
   }
 
   render() {
-    const { match, isCollectionFetching } = this.props;
+    const { match, isCollectionFetching, isCollectionLoaded } = this.props;
 
     return (
       <div className='shop-page'>
         <Route exact path={`${match.path}`} render={(props) => <CollectionOverviewWithSpinner isLoading={isCollectionFetching} {...props} />} />
         <Route 
           path={`${match.path}/:collectionId`} 
-          render={ (props) => <CollectionPageWithSpinner isLoading={isCollectionFetching} {...props}/>} />
+          render={ (props) => <CollectionPageWithSpinner isLoading={!isCollectionLoaded} {...props}/>} />
       </div>
     );
   };
 };
 
 const mapStateToProps = createStructuredSelector({
-  isCollectionFetching: selectIsCollectionFetching
+  isCollectionFetching: selectIsCollectionFetching,
+  isCollectionLoaded: selectIsCollectionsLoaded
 });
 
 const mapDispatchToProps = dispatch => ({
