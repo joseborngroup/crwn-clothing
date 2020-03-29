9a7e6d86636509a2ed1c5769e579257b47a72085
Jose Paitamala <jose.paitamala@borngroup.com>
2020-03-29 20:48:17 +0100

    Sign Out with Sagas

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -4,7 +4,9 @@ import UserActionTypes from './user.types';
 
 import {
   signInSuccess,
-  signInFailure
+  signInFailure,
+  signOutSuccess,
+  signOutFailure
 } from './user.actions';
 
 import {
@@ -55,6 +57,15 @@ export function* isUserAuthenticated() {
   }
 }
 
+export function* signOut() {
+  try {
+    yield auth.signOut();
+    yield (put(signOutSuccess()));
+  } catch(error) {
+    yield put(signOutFailure(error));
+  }
+}
+
 export function* onGoogleSignInStart() {
   yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
@@ -67,10 +78,15 @@ export function* onCheckUserSession() {
   yield takeLatest(UserActionTypes.CHECK_USER_SESSION, isUserAuthenticated);
 }
 
+export function* onSignOutStart() {
+  yield takeLatest(UserActionTypes.SIGN_OUT_START, signOut);
+}
+
 export function* userSagas() {
   yield all([
     call(onGoogleSignInStart),
     call(onEmailSignInStart),
-    call(isUserAuthenticated)
+    call(isUserAuthenticated),
+    call(onSignOutStart)
   ]);
 }
