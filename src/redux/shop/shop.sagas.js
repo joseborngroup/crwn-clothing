78575f704eebacf7d1073d23170c055297c8a34b
Jose Paitamala <jose.paitamala@borngroup.com>
2020-07-12 22:42:19 +0100

    Clear Cart Saga

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
@@ -28,4 +28,10 @@ export function* fetchCollectionsStart() {
     ShopActionsTypes.FETCH_COLLECTIONS_START, 
     fetchCollectionsAsync
   );
+}
+
+export function* shopSagas() {
+  yield all([
+    call(fetchCollectionsStart)
+  ]);
 }
