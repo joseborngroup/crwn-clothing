df7f580b02e28be07284c666ab57560115bc6ffa
Jose Paitamala <jose.paitamala@borngroup.com>
2020-07-13 20:36:21 +0100

    useEffect in our App - useEffect as ComponentDidMount

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
@@ -16,42 +16,38 @@ import { checkUserSession } from './redux/user/user.actions';
 import './App.css';
 
 
-class App extends React.Component {
+const App =  ({checkUserSession, currentUser}) => {
 
-  unsubscribeFromAuth = null;
-
-  componentDidMount() {
-
-    const { checkUserSession } = this.props;
-
+  useEffect(() => {
     checkUserSession();
-  }
+  }, [checkUserSession]);
 
-  componentWillUnmount() {
-    this.unsubscribeFromAuth();
-  }
+  // unsubscribeFromAuth = null;
 
-  render() {
-    return (
-      <div>
-        <Header />
-        <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route path='/shop' component={ShopPage} />
-          <Route path='/checkout' component={CheckoutPage} />
-          <Route exact path='/signin' 
-            render={ () => 
-              this.props.currentUser ? (
-                <Redirect to='/' />
-              ) : (
-                <SignInAndSignUpPage />
-              )
-            }
-          />
-        </Switch>  
-      </div> 
-    );
-  }
+  // componentWillUnmount() {
+  //   this.unsubscribeFromAuth();
+  // }
+
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/checkout' component={CheckoutPage} />
+        <Route exact path='/signin' 
+          render={ () => 
+            currentUser ? (
+              <Redirect to='/' />
+            ) : (
+              <SignInAndSignUpPage />
+            )
+          }
+        />
+      </Switch>  
+    </div> 
+  );
+
 }
 
 const mapStateToProps = createStructuredSelector({
